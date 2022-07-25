f20b91eb2e39d9c2eb787d61b407a8118670c417
Krzysiek Krasowski <Krzysiek.Krasowski@gmail.com>
2022-07-25 22:41:01 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+#Mod 3 Zad 1
+print("Mod 3 zad 1") 
+print()
+d = {
+    "piekarnia": ["chleb", "bułki", "pączek"],
+    "warzywniak": ["marchew", "seler", "rukola"],
+    }
+suma = 0
+for k in d:
+    temp = [v.capitalize() for v in d[k]]
+    print(f"Idę do {k.capitalize()} kupię tu naspępujące rzeczy {temp}")
+    suma = suma + len(d[k])    
+print(f"W sumie kupuję {suma} produktów")
+print()
+
+#Mod 3 Zad 2
+print("Mod 3 zad 2") 
+print()
+licz1 = [x for x in range(0,100) if not x % 5]
+licz2 = [y**3 for y in licz1]
+print(licz1)
+print(licz2)
+
